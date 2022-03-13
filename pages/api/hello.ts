20cb4c34fc17a05b15c3ef74df684ba5e9ed3d1a
Damar Albaribin <damaralbaribin@gmail.com>
2022-03-13 21:34:56 +0700

    feat : first commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,40 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import Curseforge from 'node-curseforge';
 
 type Data = {
   name: string
 }
 
-export default function handler(
+const SKEY = "$2a$10$r0OpRKUqI6P07tX2gTfWfuS4w9p6ZnDTKMRhudyCcnP8HmbDSf/Bm";
+
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+
+  let cf = new Curseforge(SKEY);
+
+  const dd: any[] = []
+
+  let mc = await cf.get_game("minecraft");
+
+  await cf.search_mods(mc, {
+    pageSize
+      : 10
+  }).then(async (mods) => {
+    for (let mod of mods) {
+      dd.push({
+        categories: mod.categories.map(e => e.name).join(","),
+        name: mod.name,
+        id: mod.id,
+        url: mod.links,
+        dl: await mod.get_files()
+      })
+    }
+  });
+
+  res.status(200).json({ dd } as any)
+
 }
